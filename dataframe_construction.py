bc0ceaecbe61726527d7bd3208966fa869eee2f3
Scott White <whiteau@fhcrc.org>
2017-09-27 15:34:35 -0700

    wrapped dataframe_construction in main() loop and returning dataframe from it
    wrapped dataframe_construction in main() loop and returning dataframe
    from it
    
    This will allow dataframe_construction.main() to be callable from other
    files to create the main data.

--- dataframe_construction.py
+++ dataframe_construction.py
@@ -1,9 +1,9 @@
 import pylidc as pl
 import pandas as pd
 import numpy as np
-
-qu = pl.query(pl.Scan) #.filter(pl.Scan.scan_id <= 200)
-data = pd.DataFrame(columns=['scan_id',
+def main():
+    qu = pl.query(pl.Scan) #.filter(pl.Scan.scan_id <= 200)
+    data = pd.DataFrame(columns=['scan_id',
                              'patient',
                              'nodule_id',
                              'annotation',
@@ -16,26 +16,31 @@ data = pd.DataFrame(columns=['scan_id',
                              'spiculation',
                              'subtlety',
                              'texture'])
-scans = [j for (i, j) in enumerate(qu)]
-i = -1
+    scans = [j for (i, j) in enumerate(qu)]
+    i = -1
 
-for scan in scans[:100]:
+    for scan in scans[:100]:
 
-    annotations = [ann for ann in scan.annotations]
+        annotations = [ann for ann in scan.annotations]
 
-    for j, annotation in enumerate(annotations):
-        i += 1
-        data.loc[i, ['scan_id']] = annotation.scan_id
-        data.loc[i, ['patient']] = scan.patient_id
-        data.loc[i, ['nodule_id']]
-        data.loc[i, ['annotation']] = j
-        data.loc[i, ['calcification']] = annotation.calcification
-        data.loc[i, ['internalStructure']] = annotation.internalStructure
-        data.loc[i, ['lobulation']] = annotation.lobulation
-        data.loc[i, ['malignancy']] = annotation.malignancy
-        data.loc[i, ['margin']] = annotation.margin
-        data.loc[i, ['sphericity']] = annotation.sphericity
-        data.loc[i, ['spiculation']] = annotation.spiculation
-        data.loc[i, ['subtlety']] = annotation.subtlety
-        data.loc[i, ['texture']] = annotation.texture
+        for j, annotation in enumerate(annotations):
+            i += 1
+            data.loc[i, ['scan_id']] = annotation.scan_id
+            data.loc[i, ['patient']] = scan.patient_id
+            data.loc[i, ['nodule_id']]
+            data.loc[i, ['annotation']] = j
+            data.loc[i, ['calcification']] = annotation.calcification
+            data.loc[i, ['internalStructure']] = annotation.internalStructure
+            data.loc[i, ['lobulation']] = annotation.lobulation
+            data.loc[i, ['malignancy']] = annotation.malignancy
+            data.loc[i, ['margin']] = annotation.margin
+            data.loc[i, ['sphericity']] = annotation.sphericity
+            data.loc[i, ['spiculation']] = annotation.spiculation
+            data.loc[i, ['subtlety']] = annotation.subtlety
+            data.loc[i, ['texture']] = annotation.texture
+    print("Data frame called 'data' has been successfully created")
 
+    return data
+
+if __name__ == "__main__":
+    main()
